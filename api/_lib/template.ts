c9922866e7b2ac66227fa3e340bda37d917b226b
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-09-06 13:46:56 +0700

    style: :art: return the style to the default style

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,32 +1,27 @@
-import { readFileSync } from "fs";
-import marked from "marked";
-import { sanitizeHtml } from "./sanitizer";
-import { ParsedRequest } from "./types";
-const twemoji = require("twemoji");
-const twOptions = { folder: "svg", ext: ".svg" };
+
+import { readFileSync } from 'fs';
+import marked from 'marked';
+import { sanitizeHtml } from './sanitizer';
+import { ParsedRequest } from './types';
+const twemoji = require('twemoji');
+const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(
-  `${__dirname}/../_fonts/Inter-Regular.woff2`
-).toString("base64");
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString(
-  "base64"
-);
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
-  "base64"
-);
+const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
+const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
+const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
 
 function getCss(theme: string, fontSize: string) {
-  let background = "white";
-  let foreground = "black";
-  let radial = "lightgray";
+    let background = 'white';
+    let foreground = 'black';
+    let radial = 'lightgray';
 
-  if (theme === "dark") {
-    background = "black";
-    foreground = "white";
-    radial = "dimgray";
-  }
-  return `
+    if (theme === 'dark') {
+        background = 'black';
+        foreground = 'white';
+        radial = 'dimgray';
+    }
+    return `
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
@@ -105,17 +100,12 @@ function getCss(theme: string, fontSize: string) {
         font-style: normal;
         color: ${foreground};
         line-height: 1.8;
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
     }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-  const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
-  return `<!DOCTYPE html>
+    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
+    return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
@@ -127,15 +117,13 @@ export function getHtml(parsedReq: ParsedRequest) {
         <div>
             <div class="spacer">
             <div class="logo-wrapper">
-                ${images
-                  .map(
-                    (img, i) =>
-                      getPlusSign(i) + getImage(img, widths[i], heights[i])
-                  )
-                  .join("")}
+                ${images.map((img, i) =>
+                    getPlusSign(i) + getImage(img, widths[i], heights[i])
+                ).join('')}
             </div>
+            <div class="spacer">
             <div class="heading">${emojify(
-              md ? marked(text) : sanitizeHtml(text)
+                md ? marked(text) : sanitizeHtml(text)
             )}
             </div>
         </div>
@@ -143,16 +131,16 @@ export function getHtml(parsedReq: ParsedRequest) {
 </html>`;
 }
 
-function getImage(src: string, width = "auto", height = "225") {
-  return `<img
+function getImage(src: string, width ='auto', height = '225') {
+    return `<img
         class="logo"
         alt="Generated Image"
         src="${sanitizeHtml(src)}"
         width="${sanitizeHtml(width)}"
         height="${sanitizeHtml(height)}"
-    />`;
+    />`
 }
 
 function getPlusSign(i: number) {
-  return i === 0 ? "" : '<div class="plus">+</div>';
+    return i === 0 ? '' : '<div class="plus">+</div>';
 }
